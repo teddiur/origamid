a2bbc5040b22e1f6daaff1dd45a5598dec7c30fc
teddiur <ryaqueiroz@gmail.com>
2021-02-27 21:29:03 -0300

    refactor components

--- src/classes/UseEffect/Product.js
+++ src/classes/UseEffect/Product.js
@@ -1,13 +1,32 @@
 import React from 'react';
 
-export const Product = ({ data }) => {
+export const Product = ({ product }) => {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    const url = `https://ranekapi.origamid.dev/json/api/produto/${product}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        window.localStorage.setItem('produto', product);
+      });
+
+    return () => {
+      setData(null);
+    };
+  }, [product]);
   return (
     <div>
-      <p>{data.nome}</p>
-      <p>R$ {data.preco}</p>
-      {data.fotos.map((foto) => (
-        <img key={foto.titulo} src={foto.src} alt={foto.titulo} />
-      ))}
+      {data && (
+        <>
+          <p>{data.nome}</p>
+          <p>R$ {data.preco}</p>
+          {data.fotos.map((foto) => (
+            <img key={foto.titulo} src={foto.src} alt={foto.titulo} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
--- src/classes/UseEffect/UseEffect.js
+++ src/classes/UseEffect/UseEffect.js
@@ -3,28 +3,16 @@ import { Product } from './Product';
 
 export const UseEffect = () => {
   const [preference, setPreference] = React.useState();
-  const [data, setData] = React.useState(null);
 
   React.useEffect(() => {
     const pref = window.localStorage.getItem('produto');
-    if (pref) setPreference(pref);
+    setPreference(pref);
   }, []);
-
-  React.useEffect(() => {
-    if (preference) {
-      const url = `https://ranekapi.origamid.dev/json/api/produto/${preference}`;
-      fetch(url)
-        .then((response) => response.json())
-        .then((json) => {
-          setData(json);
-          window.localStorage.setItem('produto', preference);
-        });
-    }
-  }, [preference]);
 
   function handleClick(event) {
     const buttonName = event.target.innerText;
-    setPreference(buttonName);
+    const pref = window.localStorage.getItem('produto');
+    if (pref !== buttonName) setPreference(buttonName);
   }
   return (
     <>
@@ -35,7 +23,7 @@ export const UseEffect = () => {
       <button style={{ margin: '5px' }} onClick={handleClick}>
         smartphone
       </button>
-      {data && <Product data={data} />}
+      <Product product={preference} />
     </>
   );
 };
