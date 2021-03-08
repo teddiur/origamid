ed8c86dcf18c9cb978bdca967d61267d9e53d983
Rodrigo Yamagushi <rodrigo.yamagushi@stormgroup.com.br>
2021-03-08 18:02:03 -0300

    loading done

--- src/classes/Router1/Produto.js
+++ src/classes/Router1/Produto.js
@@ -1,34 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import { useHead } from "../../hooks/useHead";
 
+import { Loading } from "../../components/Loading/Loading";
 import { baseURL } from "./Router1";
 
 export const Produto = () => {
   const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
   React.useEffect(() => {
     fetch(baseURL)
       .then((response) => response.json())
-      .then((json) => setData(json));
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      });
   }, []);
   useHead();
 
   return (
     <div className="product-container flex-row">
-      {data?.map((item) => {
-        return (
-          <figure className="animation-slide-in product-wrapper" key={item.id}>
-            <Link to={`produto/${item.id}`}>
-              <img
-                className="product-image"
-                src={item.fotos[0].src}
-                alt={item.nome}
-              />
-            </Link>
-            <figcaption>{item.nome}</figcaption>
-          </figure>
-        );
-      })}
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          {data?.map((item) => {
+            return (
+              <figure
+                className="animation-slide-in product-wrapper"
+                key={item.id}
+              >
+                <Link to={`produto/${item.id}`}>
+                  <img
+                    className="product-image"
+                    src={item.fotos[0].src}
+                    alt={item.nome}
+                  />
+                </Link>
+                <figcaption>{item.nome}</figcaption>
+              </figure>
+            );
+          })}
+        </>
+      )}
     </div>
   );
 };
--- src/classes/Router1/ProdutoDetalhe.js
+++ src/classes/Router1/ProdutoDetalhe.js
@@ -1,16 +1,21 @@
 import React from "react";
 import { useParams } from "react-router";
 
+import { Loading } from "../../components/Loading/Loading";
 import { baseURL } from "./Router1";
 
 export const ProdutoDetalhe = () => {
   const params = useParams();
   const [data, setData] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch(baseURL + `/${params.id}`)
       .then((response) => response.json())
-      .then((json) => setData(json));
+      .then((json) => {
+        setData(json);
+        setTimeout(() => setLoading(false), 200);
+      });
   }, [params.id]);
 
   React.useEffect(() => {
@@ -19,23 +24,29 @@ export const ProdutoDetalhe = () => {
 
   return (
     <div className="flex-row animation-slide-in">
-      {data && (
+      {loading ? (
+        <Loading />
+      ) : (
         <>
-          <div className="half-page flex-column">
-            {data.fotos?.map((foto) => (
-              <img
-                key={foto.titulo}
-                className="product-photo"
-                src={foto.src}
-                alt={foto.titulo}
-              />
-            ))}
-          </div>
-          <div className="flex-column product-description half-page">
-            <h2>{data?.nome}</h2>
-            <p className="product-price">{`R$ ${data?.preco}`}</p>
-            <p className="product-text">{data?.descricao}</p>
-          </div>
+          {data && (
+            <>
+              <div className="half-page flex-column">
+                {data.fotos?.map((foto) => (
+                  <img
+                    key={foto.titulo}
+                    className="product-photo"
+                    src={foto.src}
+                    alt={foto.titulo}
+                  />
+                ))}
+              </div>
+              <div className="flex-column product-description half-page">
+                <h2>{data?.nome}</h2>
+                <p className="product-price">{`R$ ${data?.preco}`}</p>
+                <p className="product-text">{data?.descricao}</p>
+              </div>
+            </>
+          )}
         </>
       )}
     </div>
